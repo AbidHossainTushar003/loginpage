72999c5e6318817a910d7af1cc8459bab10c41b9
Abid <abidhossaintushar003@gmail.com>
2026-06-28 06:24:18 +0600

    Initial commit

---
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.login
+
+import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        findViewById<Button>(R.id.btnFacebook).setOnClickListener {
+            startActivity(Intent(this, FacebookLogin::class.java))
+        }
+
+        findViewById<Button>(R.id.btnInstagram).setOnClickListener {
+            startActivity(Intent(this, InstraLogin::class.java))
+        }
+
+        findViewById<Button>(R.id.btnTwitter).setOnClickListener {
+            startActivity(Intent(this, XLogin::class.java))
+        }
+
+        findViewById<Button>(R.id.btnLinkedIn).setOnClickListener {
+            startActivity(Intent(this, LinkedIn::class.java))
+        }
+    }
+}
